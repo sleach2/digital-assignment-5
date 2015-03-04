4b9785fe66db67ff8b2dd088262b33e5f6d18887
sleach2 <sleach2@gmu.edu>
2015-03-04 18:41:21 -0500

    tr

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function() {
     
     function preload() {
         game.load.image( 'dirt', 'assets/dirt.png' );
-        game.load.spritesheet('player','assets/solider.png',70,81);
+        game.load.spritesheet('player','assets/solider.png',72,81);
     }
     
     var player;
