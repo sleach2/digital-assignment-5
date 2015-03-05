767f1a3bcd8acaf2b30174e44bbec55535a73402
sleach2 <sleach2@gmu.edu>
2015-03-04 19:59:45 -0500

    iu

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ window.onload = function() {
         player=game.add.sprite(0,0,'player');
         game.physics.enable(player,Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
-        player.animations.add('left', [15,16,17,18,19,20], 10, true);
-        player.animations.add('right', [22,23,24,25,26,27], 10, true);
-        player.animations.add('up', [7,8,9,10,11,12], 10, true);
-        player.animations.add('down', [1,2,3,4,5,6], 10, true);
+        player.animations.add('left', [15,16,17,18,19], 10, true);
+        player.animations.add('right', [22,23,24,25,26], 10, true);
+        player.animations.add('up', [7,8,9,10,11], 10, true);
+        player.animations.add('down', [1,2,3,4,5], 10, true);
         move = game.input.keyboard.createCursorKeys();
     }
     
