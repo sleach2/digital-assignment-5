333c979188599d89e2cc1e04bc0df14c9d5b1d56
sleach2 <sleach2@gmu.edu>
2015-03-07 22:38:12 -0500

    k

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ window.onload = function() {
     function end(h,j){
         timer.stop();
         enemies.forEachAlive(function(enemy){enemy.kill();},this);
-        //enemies.destroy(true,true);
-        //game.add.text(game.world.width/2,game.world.height/2, 16, 'Game Over', { fontSize: '64px', fill: '#000' });
+        move.destroy();
+        //game.add.text(750,350, 16, 'Game Over', { fontSize: '64px', fill: '#000' });
     }
 };
