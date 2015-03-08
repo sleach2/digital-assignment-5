5a08917150f4643b3f5ce37bbfaee3b7c21649f6
sleach2 <sleach2@gmu.edu>
2015-03-07 22:20:14 -0500

    dfg

--- js/main.js
+++ js/main.js
@@ -105,8 +105,8 @@ window.onload = function() {
     }
 
     function end(){
-        timer.stop();
+        //timer.stop();
         enemies.forEachAlive(function(enemy){enemy.kill();},this);
-        game.add.text(game.world.width/2,game.world.height/2, 16, 'Game Over', { fontSize: '32px', fill: '#000' });
+        game.add.text(game.world.width/2,game.world.height/2, 16, 'Game Over', { fontSize: '64px', fill: '#000' });
     }
 };
