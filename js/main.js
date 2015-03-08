28f6e3f69a7dbb7c3ecdcc188c24d4ff3c7f071f
sleach2 <sleach2@gmu.edu>
2015-03-07 22:58:43 -0500

    k

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ window.onload = function() {
     
     function preload() {
         game.load.image( 'dirt', 'assets/dirt.png' );
-        //game.load.spritsheet('player','assets/solider.png',72,81);
         game.load.image('player', 'assets/man.PNG');
         game.load.image('bullet','assets/bullet.png');
         game.load.image('a','assets/player1.png');
+        game.load.audio('bks','assets/eerie.mp3');
     }
     
     var player;
@@ -21,19 +21,20 @@ window.onload = function() {
     var scoreText;
     var enemies;
     var yui=0;
+    var music;
 
     
     function create() {
+        music=game.add.audio('bks');
+        music.play('',0,0.5,true);
+
         game.add.tileSprite(0,0,2000,2000,'dirt');
 
         player=game.add.sprite(0,100,'player');
         game.physics.enable(player,Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
         player.anchor.setTo(0.5,0.5);
-        /*player.animations.add('left', [12,13,14,15,16,17], 10, true);
-        player.animations.add('right', [19,20,21,22,23,24], 10, true);
-        player.animations.add('up', [7,8,9,10,11], 10, true);
-        player.animations.add('down', [1,2,3,4,5], 10, true);*/
+
         move = game.input.keyboard.createCursorKeys();
 
         bullets = game.add.group();
